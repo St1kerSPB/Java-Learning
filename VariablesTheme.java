74dbcf79ce2144a91f64cee91c34cf435acb0152
St1kerSPB <andrey1007_90@mail.ru>
2023-09-30 22:56:50 +0300

    Исправление в коде порядка объявления переменных

--- VariablesTheme.java
+++ VariablesTheme.java
@@ -1,21 +1,20 @@
 public class VariablesTheme {
-
     public static void main(String[] args) {
         System.out.println("1. Вывод характеристик компьютера.\n");
         byte memory = 4;
         short ram = 2;
-        System.out.println("ОЗУ: " + ram + "x" + memory + "GB");
         int mhz = 3470;
         byte cpu = 5;
-        char model = 'i';
-        System.out.println("ЦП: " + model + cpu + "-" + mhz);
+        char model = 'i'; 
         long ssd = 106522841088L;
-        System.out.println("SSD: " + ssd + " байт");
         float hdd1 = 1.81f;
-        System.out.println("HDD: " + hdd1 + "TB");
         double hdd2 = 935.2d;
-        System.out.println("HDD: " + hdd2 + "GB");
         boolean os = true;
+        System.out.println("ОЗУ: " + ram + "x" + memory + "GB");
+        System.out.println("ЦП: " + model + cpu + "-" + mhz);
+        System.out.println("SSD: " + ssd + " байт");
+        System.out.println("HDD: " + hdd1 + "TB");
+        System.out.println("HDD: " + hdd2 + "GB");
         System.out.println("Windows activate = " + os + "\n");
 
         System.out.println("2. Расчет стоимости товара со скидкой.\n");
@@ -36,18 +35,18 @@ public class VariablesTheme {
 
         System.out.println("4. Вывод min и max значений целых числовых типов.\n");
         byte maxByte = 127;
+        short maxShort = 32767;
+        int maxInt = 2147483647;
+        long maxLong = 9223372036854775807L;
         System.out.println("byte " + maxByte);
         System.out.println(++maxByte);
         System.out.println(--maxByte + "\n");
-        short maxShort = 32767;
         System.out.println("short " + maxShort);
         System.out.println(++maxShort);
         System.out.println(--maxShort + "\n");
-        int maxInt = 2147483647;
         System.out.println("int " + maxInt);
         System.out.println(++maxInt);
         System.out.println(--maxInt + "\n");
-        long maxLong = 9223372036854775807L;
         System.out.println("long " + maxLong);
         System.out.println(++maxLong);
         System.out.println(--maxLong + "\n");
@@ -77,18 +76,18 @@ public class VariablesTheme {
         System.out.println("6. Вывод символов и их кодов.\n");
         int codeDollar = 36;
         char dollar = '$';
-        System.out.println(codeDollar + " " + dollar);
         int codeAsterisk = 42;
         char asterisk = '*';
-        System.out.println(codeAsterisk + " " + asterisk);
         int codeAtSign = 64;
         char atSign = '@';
-        System.out.println(codeAtSign + " " + atSign);
         int codeBar = 124;
         char bar = '|';
-        System.out.println(codeBar + " " + bar);
         int codeTilde = 126;
         char tilde = '~';
+        System.out.println(codeDollar + " " + dollar);
+        System.out.println(codeAsterisk + " " + asterisk);
+        System.out.println(codeAtSign + " " + atSign);
+        System.out.println(codeBar + " " + bar);
         System.out.println(codeTilde + " " + tilde + "\n");
 
         System.out.println("7. Вывод в консоль ASII-арт Дюка.\n");
