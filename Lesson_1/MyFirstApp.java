746cbc3ad56c0affc5dbaf69c5e7545f7313e69f
St1kerSPB <andrey1007_90@mail.ru>
2023-10-05 20:28:47 +0300

    Добавил файл .gitignore с шаблоном *.class, исправил ошибку в названии класса MyFirstApp.java

--- Lesson_1/MyFirstApp.java
+++ Lesson_1/MyFirstApp.java
@@ -1,4 +1,4 @@
-public class MyFirtsApp {
+public class MyFirstApp {
     public static void main(String[] args) {
         System.out.print("Написано однажды, ");
         System.out.println("работает везде!");
